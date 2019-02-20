c134dc4fe7df82ec6483237a19cb695f7414846e
zdrobenko <zdrobenko@carthage.edu>
2019-02-20 15:15:52 -0600

    Fixed the bug

--- Git.cpp
+++ Git.cpp
@@ -6,13 +6,13 @@ void power(int base, int exponent);
 
 int main() {
 
+	power(4, 2);
 	return 0;
 }
 
 void power(int base, int exponent) {
 	int number;
 
-	number = pow(num, 2);
 	cout << "Base number: " << base << endl;
 	cout << "Exponent: " << exponent << endl;
 }
